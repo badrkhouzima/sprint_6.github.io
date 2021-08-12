1b7aaa2d74f77788029fe292973d272cda3a52bf
badr <badrhand@gmail.com>
2021-08-12 02:08:39 +0200

    exercise_1 level1

---
+++ src/components/scene/Scene.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Scene = () => (
+  <div>
+    Nuestro héroe estaba flotando por el espacio sideral cuando a lo lejos
+    divisó una nave espacial
+  </div>
+);
+
+
+export default Scene;
